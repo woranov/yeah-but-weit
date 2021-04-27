woranov/yeah-but-weit

commit 3aa037c0b38b761d86658bfdd174e8c093b374ca

404 early on invalid twitch channel names and emote codes

src/emotes/ttv.ts
@@ -1,5 +1,6 @@
 import { BaseChannelEmote, BaseEmote } from "./base";
 import { CACHE_TTL } from "../config";
+import { checkEmoteCode } from "../twitch";
 
 
 class GlobalEmote extends BaseEmote {
@@ -164,6 +165,10 @@ async function find(
 ): Promise<Emote | null> {
   // FIXME: fucking mess (!)
 
+  if (!checkEmoteCode({ emoteCode: code, caseSensitive: false })) {
+    return null;
+  }
+
   try {
     if (channel !== null) {
       const channelEmotes = await list(channel);
@@ -180,14 +185,17 @@ async function find(
           return emote;
         }
       }
-    } else {
+    } else if (checkEmoteCode( { emoteCode: code, caseSensitive: true })) {
       const emote = await findCode(code);
       if (emote) {
         return emote;
       }
     }
   } catch (e) {
-    if (e instanceof TwitchEmotesApiIsFuckedAgain) {
+    if (
+      e instanceof TwitchEmotesApiIsFuckedAgain
+      && checkEmoteCode( { emoteCode: code, caseSensitive: true })
+    ) {
       const emote = await findCode(code);
       if (emote !== null && channel !== null) {
         if (

src/handlers/emotes.ts
@@ -1,6 +1,6 @@
 import { Request } from "itty-router";
 
-import { fetchTwitchChannelId } from "../twitch";
+import { checkChannelName, fetchTwitchChannelId } from "../twitch";
 import { BaseEmote, BaseChannelEmote, find } from "../emotes";
 import { createHtml, notFoundHandler } from "./common";
 
@@ -24,6 +24,10 @@ export const makeHandler = (provider: EmoteProviderName | null = null) => {
     let channel: Channel | null = null;
 
     if (channelName) {
+      if (!checkChannelName(channelName)) {
+        return notFoundHandler();
+      }
+
       const channelId = await fetchTwitchChannelId(channelName);
 
       if (channelId === null)

src/twitch.ts
@@ -1,4 +1,9 @@
-export async function fetchTwitchChannelId(
+const CHANNEL_NAME_REGEX = /^[a-zA-Z0-9_]{4,25}$/;
+const CASE_SENSITIVE_EMOTE_CODE_REGEX = /^[a-z0-9]{3,}[A-Z0-9]\w*$/;
+const CASE_INSENSITIVE_EMOTE_CODE_REGEX = /^[a-zA-Z0-9]{3,}\w+$/;
+
+
+async function fetchTwitchChannelId(
   channel: string,
 ): Promise<number | null> {
   try {
@@ -20,3 +25,22 @@ export async function fetchTwitchChannelId(
     return null;
   }
 }
+
+
+function checkChannelName(channelName: string): boolean {
+  return CHANNEL_NAME_REGEX.test(channelName);
+}
+
+
+function checkEmoteCode({ emoteCode, caseSensitive }: {
+  emoteCode: string, caseSensitive: boolean
+}): boolean {
+  return (
+    caseSensitive
+      ? CASE_SENSITIVE_EMOTE_CODE_REGEX
+      : CASE_INSENSITIVE_EMOTE_CODE_REGEX
+  ).test(emoteCode);
+}
+
+
+export { fetchTwitchChannelId, checkChannelName, checkEmoteCode };

commit 1b36bf4d99dd24398cd8dab5287cc8e958c05bfa

Fix bug introduced in 3aa037c0

src/emotes/ttv.ts
@@ -170,7 +170,7 @@ async function find(
   }
 
   try {
-    if (channel !== null) {
+    if (channel !== null && checkEmoteCode({ emoteCode: code, caseSensitive: true })) {
       const channelEmotes = await list(channel);
       const foundInChannelEmotes = (
         channelEmotes && channelEmotes.find(
@@ -185,7 +185,7 @@ async function find(
           return emote;
         }
       }
-    } else if (checkEmoteCode({ emoteCode: code, caseSensitive: true })) {
+    } else {
       const emote = await findCode(code);
       if (emote) {
         return emote;