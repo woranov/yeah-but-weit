woranov/yeah-but-weit

commit b389ef4252a6febfacca19a1e54ee38278261d01

Add support for global bttv/ffz emotes

src/emotes/bttv.ts
@@ -1,9 +1,24 @@
-import { BaseChannelEmote } from "./base";
+import { BaseChannelEmote, BaseGlobalEmote } from "./base";
 import { formatNumber } from "../formatting";
 import { CACHE_TTL, LONG_CACHE_TTL } from "../config";
 
 
-class Emote extends BaseChannelEmote {
+class GlobalEmote extends BaseGlobalEmote {
+  get description(): string {
+    return "Global BTTV Emote";
+  }
+
+  get imageUrl(): string {
+    return `https://cdn.betterttv.net/emote/${this.id}/3x`;
+  }
+
+  get infoUrl(): string {
+    return `https://betterttv.com/emotes/${this.id}`;
+  }
+}
+
+
+class ChannelEmote extends BaseChannelEmote {
   readonly usageCount: number | null;
   readonly #isShared: boolean | null;
 
@@ -43,7 +58,34 @@ class Emote extends BaseChannelEmote {
 }
 
 
-async function list(channel: Channel): Promise<Emote[] | null> {
+type Emote = GlobalEmote | ChannelEmote;
+
+
+async function listGlobal(): Promise<GlobalEmote[] | null> {
+  const key = `list:bttv:global`;
+
+  let data = <BttvEmoteEntry[] | null>await EMOTES.get(key, "json");
+
+  if (!data) {
+    const response = await fetch(
+      "https://api.betterttv.net/3/cached/emotes/global",
+    );
+
+    if (response.ok) {
+      data = <BttvEmoteEntry[]>await response.json();
+      await EMOTES.put(key, JSON.stringify(data), { expirationTtl: CACHE_TTL });
+    }
+  }
+
+  if (data) {
+    return data.map(e => new GlobalEmote({ id: e.id, code: e.code }));
+  } else {
+    return null;
+  }
+}
+
+
+async function listChannel(channel: Channel): Promise<ChannelEmote[] | null> {
   const key = `list:bttv:${channel.id}`;
 
   let data = <BttvEmoteListResult | null>await EMOTES.get(key, "json");
@@ -64,10 +106,10 @@ async function list(channel: Channel): Promise<Emote[] | null> {
       ...data.sharedEmotes.map((
         { id, code, user: { displayName: creatorDisplayName } }: BttvSharedEmoteEntry,
       ) => {
-        return new Emote({ id, code, creatorDisplayName, isShared: true });
+        return new ChannelEmote({ id, code, creatorDisplayName, isShared: true });
       }),
       ...data.channelEmotes.map(({ id, code }: BttvChannelEmoteEntry) => {
-        return new Emote({ id, code, creatorDisplayName: channel.name, isShared: false });
+        return new ChannelEmote({ id, code, creatorDisplayName: channel.name, isShared: false });
       }),
     ];
   } else {
@@ -78,7 +120,7 @@ async function list(channel: Channel): Promise<Emote[] | null> {
 
 async function listTop({ count = 4_500, force = false }: {
   count?: number, force?: boolean
-}): Promise<Emote[]> {
+}): Promise<ChannelEmote[]> {
   const key = "list:bttv:top";
   const perPage = 100;
   let emoteData = force
@@ -110,13 +152,13 @@ async function listTop({ count = 4_500, force = false }: {
       );
   }
 
-  return emoteData.map(e => new Emote({
+  return emoteData.map(e => new ChannelEmote({
     id: e.emote.id, code: e.emote.code, creatorDisplayName: e.emote.user.displayName, usageCount: e.total + 1,
   }));
 }
 
 
-async function findCode(code: string, considerOldestN: number = 5): Promise<Emote | null> {
+async function findCode(code: string, considerOldestN: number = 5): Promise<ChannelEmote | null> {
   const key = `search:bttv:${code}`;
   const perPage = 100;
   let emoteData: BttvEmoteSearchResultEntry[] | null = <BttvEmoteSearchResultEntry[] | null>await EMOTES.get(key, "json");
@@ -165,7 +207,7 @@ async function findCode(code: string, considerOldestN: number = 5): Promise<Emot
         currMaxCount = count;
       }
     }
-    return new Emote({
+    return new ChannelEmote({
       id: currBestEmote!.id,
       code: currBestEmote!.code,
       creatorDisplayName: currBestEmote!.user.displayName,
@@ -180,22 +222,27 @@ async function findCode(code: string, considerOldestN: number = 5): Promise<Emot
 async function find(
   { code, channel = null }: { code: string, channel: Channel | null },
 ): Promise<Emote | null> {
-  if (channel === null) {
+  let emote = null;
+  {
+    const globalEmotes = await listGlobal();
+    if (globalEmotes) {
+      emote = globalEmotes.find(e => e.code.toLowerCase() == code.toLowerCase()) ?? null;
+    }
+  }
+  if (!emote && channel) {
+    const channelEmotes = await listChannel(channel);
+    if (channelEmotes) {
+      emote = channelEmotes.find(e => e.code.toLowerCase() == code.toLowerCase()) ?? null;
+    }
+  }
+  if (!emote) {
     const trendingEmotes = await listTop({});
-    let emote = trendingEmotes.find(e => e.code.toLowerCase() == code.toLowerCase()) ?? null;
+    emote = trendingEmotes.find(e => e.code.toLowerCase() == code.toLowerCase()) ?? null;
     if (!emote) {
       emote = await findCode(code);
     }
-    if (emote) {
-      return emote;
-    }
-  } else {
-    const channelEmotes = await list(channel);
-    if (channelEmotes) {
-      return channelEmotes.find(e => e.code.toLowerCase() == code.toLowerCase()) ?? null;
-    }
   }
-  return null;
+  return emote;
 }
 
-export { Emote, listTop, find };
+export { ChannelEmote, GlobalEmote, find, listTop };

src/emotes/ffz.ts
@@ -1,9 +1,37 @@
-import { BaseChannelEmote } from "./base";
+import { BaseChannelEmote, BaseGlobalEmote } from "./base";
 import { formatNumber } from "../formatting";
 import { CACHE_TTL } from "../config";
 
 
-class Emote extends BaseChannelEmote {
+class GlobalEmote extends BaseGlobalEmote {
+  readonly #maxScale: number;
+
+  constructor(
+    { maxScale, ...rest }: {
+      id: number,
+      code: string,
+      maxScale: number,
+    },
+  ) {
+    super(rest);
+    this.#maxScale = maxScale;
+  }
+
+  get description(): string {
+    return "Global FFZ Emote";
+  }
+
+  get imageUrl(): string {
+    return `https://cdn.frankerfacez.com/emote/${this.id}/${this.#maxScale}`;
+  }
+
+  get infoUrl(): string {
+    return `https://www.frankerfacez.com/emoticon/${this.id}`;
+  }
+}
+
+
+class ChannelEmote extends BaseChannelEmote {
   readonly usageCount: number;
   readonly #maxScale: number;
 
@@ -35,7 +63,44 @@ class Emote extends BaseChannelEmote {
 }
 
 
-async function list(channel: Channel): Promise<Emote[] | null> {
+type Emote = GlobalEmote | ChannelEmote;
+
+async function listGlobal(): Promise<GlobalEmote[] | null> {
+  const key = `list:ffz:global`;
+
+  let data = <FfzGlobalEmoteListResult | null>await EMOTES.get(key, "json");
+
+  if (!data) {
+    const response = await fetch(
+      "https://api.frankerfacez.com/v1/set/global",
+    );
+    if (response.ok) {
+      data = <FfzGlobalEmoteListResult>await response.json();
+      await EMOTES.put(key, JSON.stringify(data), { expirationTtl: CACHE_TTL });
+    }
+  }
+  if (data) {
+    const globalEmotes: GlobalEmote[] = [];
+    for (const setId of [...data.default_sets, ...Object.keys(data.users)]) {
+      data.sets[setId.toString()].emoticons.map(
+        ({ id, name: code, urls }: FfzEmoteEntry) => {
+          const maxScale = Math.max(
+            ...Object.keys(urls).map(scale => parseInt(scale)),
+          );
+          globalEmotes.push(
+            new GlobalEmote({ id, code, maxScale }),
+          );
+        },
+      );
+    }
+    return globalEmotes;
+  } else {
+    return null;
+  }
+}
+
+
+async function listChannel(channel: Channel): Promise<ChannelEmote[] | null> {
   const key = `list:ffz:${channel.id}`;
 
   let data = <FfzEmoteListResult | null>await EMOTES.get(key, "json");
@@ -55,7 +120,7 @@ async function list(channel: Channel): Promise<Emote[] | null> {
         const maxScale = Math.max(
           ...Object.keys(urls).map(scale => parseInt(scale)),
         );
-        return new Emote({ id, code, creatorDisplayName: display_name, maxScale, usageCount: usage_count });
+        return new ChannelEmote({ id, code, creatorDisplayName: display_name, maxScale, usageCount: usage_count });
       },
     );
   } else {
@@ -64,7 +129,7 @@ async function list(channel: Channel): Promise<Emote[] | null> {
 }
 
 
-async function findCode(code: string): Promise<Emote[] | null> {
+async function findCode(code: string): Promise<ChannelEmote[] | null> {
   const key = `search:ffz:${code.toLowerCase()}`;
 
   let data = <FfzEmoteSearchResult | null>await EMOTES.get(key, "json");
@@ -85,7 +150,7 @@ async function findCode(code: string): Promise<Emote[] | null> {
         const maxScale = Math.max(
           ...Object.keys(urls).map(scale => parseInt(scale)),
         );
-        return new Emote({ id, code, creatorDisplayName: display_name, maxScale, usageCount: usage_count });
+        return new ChannelEmote({ id, code, creatorDisplayName: display_name, maxScale, usageCount: usage_count });
       });
     if (withMatchingCode.length >= 1) {
       return withMatchingCode;
@@ -98,25 +163,33 @@ async function findCode(code: string): Promise<Emote[] | null> {
 }
 
 
-// noinspection JSUnusedGlobalSymbols
+// noinspection JSUnusedGlobalSymbols, DuplicatedCode
 async function find(
   { code, channel = null }: { code: string, channel: Channel | null },
 ): Promise<Emote | null> {
-  if (channel === null) {
+  let emote = null;
+  {
+    const globalEmotes = await listGlobal();
+    if (globalEmotes) {
+      emote = globalEmotes.find(e => e.code.toLowerCase() == code.toLowerCase()) ?? null;
+    }
+  }
+  if (!emote && channel) {
+    const channelEmotes = await listChannel(channel);
+    if (channelEmotes) {
+      emote = channelEmotes.find(e => e.code.toLowerCase() == code.toLowerCase()) ?? null;
+    }
+  }
+  if (!emote) {
     const result = await findCode(code);
     if (result && result.length >= 1) {
       const candidateEmote = result[0];
       if (candidateEmote.code.toLowerCase() == code.toLowerCase()) {
-        return candidateEmote;
+        emote = candidateEmote;
       }
     }
-  } else {
-    const channelEmotes = await list(channel);
-    if (channelEmotes) {
-      return channelEmotes.find(e => e.code.toLowerCase() == code.toLowerCase()) ?? null;
-    }
   }
-  return null;
+  return emote;
 }
 
-export { Emote, find };
+export { ChannelEmote, GlobalEmote, find };

src/emotes/base.ts
@@ -20,6 +20,10 @@ export abstract class BaseEmote {
 }
 
 
+export abstract class BaseGlobalEmote extends BaseEmote {
+}
+
+
 export abstract class BaseChannelEmote extends BaseEmote {
   readonly creatorDisplayName: string;
 

src/emotes/index.ts
@@ -1,7 +1,7 @@
 import * as Ttv from "./ttv";
 import * as Bttv from "./bttv";
 import * as Ffz from "./ffz";
-import { BaseChannelEmote, BaseEmote } from "./base";
+import { BaseChannelEmote, BaseEmote, BaseGlobalEmote } from "./base";
 
 
 const EMOTE_PROVIDERS = {
@@ -21,7 +21,7 @@ async function find(
   let candidates: BaseEmote[] = [];
   for (const provider of providers) {
     const emote = await EMOTE_PROVIDERS[provider].find({ code, channel });
-    if (emote && provider == "ttv") {
+    if (emote && (provider == "ttv" || emote instanceof BaseGlobalEmote)) {
       return emote;
     } else if (emote) {
       candidates.push(emote);
@@ -31,10 +31,10 @@ async function find(
     let mostPopular = null;
     let maxCount = -1;
     for (const candidate of candidates) {
-      if (candidate instanceof Bttv.Emote && candidate.usageCount === null) {
+      if (candidate instanceof Bttv.ChannelEmote && candidate.usageCount === null) {
         return candidate;
-      } else if ((<Bttv.Emote | Ffz.Emote>candidate).usageCount! > maxCount) {
-        maxCount = (<Bttv.Emote | Ffz.Emote>candidate).usageCount!;
+      } else if ((<Bttv.ChannelEmote | Ffz.ChannelEmote>candidate).usageCount! > maxCount) {
+        maxCount = (<Bttv.ChannelEmote | Ffz.ChannelEmote>candidate).usageCount!;
         mostPopular = candidate;
       }
     }

src/emotes/ttv.ts
@@ -1,9 +1,9 @@
-import { BaseChannelEmote, BaseEmote } from "./base";
+import { BaseChannelEmote, BaseGlobalEmote } from "./base";
 import { CACHE_TTL } from "../config";
 import { checkEmoteCode } from "../twitch";
 
 
-class GlobalEmote extends BaseEmote {
+class GlobalEmote extends BaseGlobalEmote {
   get description(): string {
     return "Global Twitch Emote";
   }
@@ -215,4 +215,4 @@ async function find(
 }
 
 
-export { Emote, find };
+export { ChannelEmote, GlobalEmote, find };

src/emotes/types.d.ts
@@ -99,6 +99,17 @@ interface FfzEmoteListResult {
 }
 
 
+interface FfzGlobalEmoteListResult {
+  default_sets: number[];
+  users: { [key: string]: string[] };
+  sets: {
+    [key: string]: {
+      emoticons: FfzEmoteEntry[];
+    };
+  };
+}
+
+
 interface FfzEmoteSearchResult {
   emoticons: FfzEmoteEntry[];
 }

commit 5d6264e14af5de08db70d75c34d299a4cbe44fc7

Fix bug introduced in b389ef4

src/emotes/bttv.ts
@@ -254,7 +254,7 @@ async function find(
       emote = preferCaseSensitiveFind(channelEmotes, code);
     }
   }
-  if (!emote) {
+  if (!emote && !channel) {
     const trendingEmotes = await listTop({});
     emote = preferCaseSensitiveFind(trendingEmotes, code);
     if (!emote) {

src/emotes/ffz.ts
@@ -208,7 +208,7 @@ async function find(
       emote = channelEmotes.find(e => e.code.toLowerCase() == code.toLowerCase()) ?? null;
     }
   }
-  if (!emote) {
+  if (!emote && !channel) {
     const result = await findCode(code);
     if (result && result.length >= 1) {
       emote = preferCaseSensitiveFind(result, code);